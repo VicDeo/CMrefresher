76cf6562a4c6bfc7d8d9145b5c060a82f1b7c8da
VicDeo <victor.dubiniuk@gmail.com>
2011-10-25 00:57:03 +0300

    Main activity cleanup

--- src/com/deo/cmrefresher/CMrefresher.java
+++ src/com/deo/cmrefresher/CMrefresher.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.text.SimpleDateFormat;
-import java.text.ParseException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import android.app.ListActivity;
@@ -40,28 +39,23 @@ public class CMrefresher extends ListActivity {
         ArrayList<Message> list = new ArrayList<Message>();
         TextView textView = (TextView) findViewById(R.id.status);
         try {
-            try {
-                java.lang.Process p = Runtime.getRuntime().exec("getprop ro.modversion");
-                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                line = input.readLine();
-                input.close();
-                subprop = line.split("-");
-                isCyanogen = subprop[0].contentEquals("CyanogenMod") && subprop[1].contentEquals("7");
+            java.lang.Process p = Runtime.getRuntime().exec("getprop ro.modversion");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            line = input.readLine();
+            input.close();
+            subprop = line.split("-");
+            isCyanogen = subprop[0].contentEquals("CyanogenMod") && subprop[1].contentEquals("7");
 
-                if (isCyanogen) {
-                    buildType = subprop[3];
-                    SimpleDateFormat inputFormatter = new SimpleDateFormat("MMddyyyy");
-                    buildDate = inputFormatter.parse(subprop[2]);
-                    product = subprop[4].toLowerCase();
+            if (isCyanogen) {
+                buildType = subprop[3];
+                SimpleDateFormat inputFormatter = new SimpleDateFormat("MMddyyyy");
+                buildDate = inputFormatter.parse(subprop[2]);
+                product = subprop[4].toLowerCase();
 
-                    SimpleDateFormat outputFormatter = new SimpleDateFormat("dd-MMM-yyyy");
-                    textView.setText("Cyanogen 7 " + buildType + " build on " + outputFormatter.format(buildDate) + " for " + product);
-                } else {
-                    textView.setText("The current firmware is not Cyanogen 7");
-                }
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                SimpleDateFormat outputFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+                textView.setText("Cyanogen 7 " + buildType + " build on " + outputFormatter.format(buildDate) + " for " + product);
+            } else {
+                textView.setText("The current firmware is not Cyanogen 7");
             }
 
             AndroidSaxFeedParser feedParser = new AndroidSaxFeedParser("http://cm-nightlies.appspot.com/rss?device=" + product);
@@ -69,44 +63,40 @@ public class CMrefresher extends ListActivity {
 
             Changelog changelog;
 
-            try {
-                changelog = new Changelog("http://cm-nightlies.appspot.com/changelog/?device=" + product);
-                JSONArray nightlies = changelog.getData();
 
-                int i;
-                for (i = 0; i < nightlies.length(); i++) {
-                    JSONObject commit = nightlies.getJSONObject(i);
-                    String updated = commit.getString("last_updated");
-                    SimpleDateFormat inputFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    try {
-                        Date updatedAt = inputFormatter.parse(updated.trim());
-                        Message msg = new Message();
-                        Message targetMsg = new Message();
-                        Iterator iter = list.iterator();
+            changelog = new Changelog("http://cm-nightlies.appspot.com/changelog/?device=" + product);
+            JSONArray nightlies = changelog.getData();
 
-                        while (iter.hasNext()) {
-                            msg = (Message) iter.next();
-                            if (updatedAt.after(msg.getDateObj())) {
-                                break;
-                            }
-                            targetMsg = msg;
-                        }
+            int i;
+            for (i = 0; i < nightlies.length(); i++) {
+                JSONObject commit = nightlies.getJSONObject(i);
+                String updated = commit.getString("last_updated");
+                SimpleDateFormat inputFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-                        if (targetMsg != msg) {
-                            targetMsg.setDescription(targetMsg.getDescription()
-                                    + "\n" + commit.getString("project") + ": "
-                                    + commit.getString("subject") + "\n");
-                        }
+                Date updatedAt = inputFormatter.parse(updated.trim());
+                Message msg = new Message();
+                Message targetMsg = new Message();
+                Iterator iter = list.iterator();
 
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
+                while (iter.hasNext()) {
+                    msg = (Message) iter.next();
+                    if (updatedAt.after(msg.getDateObj())) {
+                        break;
                     }
-
+                    targetMsg = msg;
                 }
 
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                if (targetMsg != msg) {
+                    targetMsg.setDescription(targetMsg.getDescription()
+                            + "\n" + commit.getString("project") + ": "
+                            + commit.getString("subject") + "\n");
+                }
+
+
+
             }
+
+
 
             ListAdapter adapter = new ArrayAdapter<Message>(
                     this, R.layout.list, R.id.title, list);
@@ -129,14 +119,11 @@ public class CMrefresher extends ListActivity {
                     }).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
 
                         public void onClick(DialogInterface dialog, int id) {
-                            try {
-                                Intent downloadIntent = new Intent(CMrefresher.this, LoaderService.class);
-                                downloadIntent.putExtra("title", message.getTitle());
-                                downloadIntent.putExtra("link", message.getLink().toString());
-                                startService(downloadIntent);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+
+                            Intent downloadIntent = new Intent(CMrefresher.this, LoaderService.class);
+                            downloadIntent.putExtra("title", message.getTitle());
+                            downloadIntent.putExtra("link", message.getLink().toString());
+                            startService(downloadIntent);
                             dialog.cancel();
                             dialog.dismiss();
                         }
@@ -148,7 +135,7 @@ public class CMrefresher extends ListActivity {
 
                 }
             });
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             AlertDialog alertDialog = new AlertDialog.Builder(this).create();
             alertDialog.setTitle("Sorry...");
             alertDialog.setMessage(e.getMessage());
